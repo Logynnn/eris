4c01ab803f2536ade13c45b174569fe5672d050f
Caio Alexandre <contato.kaiyo@gmail.com>
2021-01-05 19:45:00 -0300

    Sistema de carregar extensões automaticamente

--- bellatrix/bot.py
+++ bellatrix/bot.py
@@ -1,3 +1,7 @@
+import os
+import re
+import logging
+
 import discord
 from discord.ext import commands
 
@@ -7,3 +11,32 @@ from discord.ext import commands
 class Bellatrix(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix='b/', intents=discord.Intents.all())
+        
+        self.all_extensions = []
+        self.logger = logging.getLogger('bellatrix')
+        
+        for root, _, files in os.walk('extensions'):
+            for file in files:
+                path = os.path.join(root, file)
+
+                if not os.path.isfile(path):
+                    continue
+
+                path, ext = os.path.splitext(path)
+                if ext != '.py':
+                    continue
+
+                # regex é um ser muito estranho.
+                extension = re.sub('\\\\|\/', '.', path)
+                self.all_extensions.append(extension)
+
+    async def on_ready(self):
+        for ext in self.all_extensions:
+            try:
+                self.load_extension(ext)
+            except Exception as e:
+                self.logger.exception(f'Extension \'{ext}\' could not be loaded.')
+            else:
+                self.logger.info(f'Extension \'{ext}\' has been loaded.')
+
+        print(f'Online com {len(self.users)} usuários')
