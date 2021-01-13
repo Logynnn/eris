859e1cde50b033a6aa6c7a4826552e890fef2914
Caio Alexandre <contato.kaiyo@gmail.com>
2021-01-13 12:31:31 -0300

    Context.reply recebe keyword-arguments

--- bellatrix/utils/context.py
+++ bellatrix/utils/context.py
@@ -4,12 +4,12 @@ from .embed import Embed
 
 
 class Context(commands.Context):
-    def get_embed(self, content: str) -> Embed:
+    def get_embed(self, content: str=None, **kwargs) -> Embed:
         '''Cria uma ``Embed`` automaticamente e a retorna.'''
         author = {'icon_url': self.author.avatar_url, 'name': self.author.display_name}
         color = self.guild.me.color
-        return Embed(description=content, author=author, color=color)
+        return Embed(description=content, author=author, color=color, **kwargs)
 
-    async def reply(self, content: str):
+    async def reply(self, content: str=None, **kwargs):
         '''Este método foi sobrescrito para enviar ``Embed`` automaticamente.'''
-        await super().reply(embed=self.get_embed(content))
+        return await super().reply(embed=self.get_embed(content, **kwargs))
