123c8bae524cc0d318abfdf7f847ee8b9affc9dd
Caio Alexandre <contato.kaiyo@gmail.com>
2021-01-13 12:30:50 -0300

    Melhoria no construtor de embeds

--- bellatrix/utils/embed.py
+++ bellatrix/utils/embed.py
@@ -5,40 +5,20 @@ import discord
 class Embed(discord.Embed):
     def __init__(self, **kwargs):
         options = {
-            'fields': {
-                'type': list,
-                'function': lambda args: map(lambda fields: self.add_field(**fields), args)
-            },
-            'footer': {
-                'type': dict,
-                'function': lambda args: self.set_footer(**args)
-            },
-            'author': {
-                'type': dict,
-                'function': lambda args: self.set_author(**args)
-            },
-            'image': {
-                'type': str,
-                'function': lambda url: self.set_image(url=url)
-            },
-            'thumbnail': {
-                'type': str,
-                'function': lambda url: self.set_thumbnail(url=url)
-            }
+            'fields': lambda args: [self.add_field(**fields) for fields in args],
+            'footer': lambda args: self.set_footer(**args),
+            'author': lambda args: self.set_author(**args),
+            'image': lambda url: self.set_image(url=url),
+            'thumbnail': lambda url: self.set_thumbnail(url=url)
         }
 
         for option in options:
             embed_attr = kwargs.get(option)
-            option_attr = options.get(option)
+            func = options.get(option)
 
             if not embed_attr:
                 continue
 
-            if not isinstance(embed_attr, option_attr['type']):
-                option_type = option_attr['type'].__name__
-                embed_type = embed_attr.__name__
-                raise TypeError(f'{option} type must be {option_type}, not {embed_type}')
-
-            option_attr['function'](embed_attr)
+            func(embed_attr)
 
         super().__init__(**kwargs)
