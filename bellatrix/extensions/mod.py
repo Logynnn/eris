0fd5003e1d3b0b5afb696d12912756f78e0d2f18
Caio Alexandre <contato.kaiyo@gmail.com>
2021-01-13 21:35:08 -0300

    Comando de silenciamento

--- bellatrix/extensions/mod.py
+++ bellatrix/extensions/mod.py
@@ -1,14 +1,18 @@
 import typing
 
 import discord
+import humanize
 from discord.ext import commands
 
 from utils import database
 from utils.menus import PunishmentMenu
 from utils.embed import Embed
+from utils.time import FutureTime
+from .reminder import Timer
 
 
 LOG_CHANNEL_ID = 798013309617176587
+MUTE_ROLE_ID = 799064942048444437
 
 class PunishmentImage(database.Table, table_name='punishment_images'):
     user_id = database.Column(database.Integer(big=True), primary_key=True)
@@ -18,7 +22,9 @@ class Mod(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.cosmic = bot.cosmic
+
         self.log_channel = bot.cosmic.get_channel(LOG_CHANNEL_ID)
+        self.mute_role = bot.cosmic.get_role(MUTE_ROLE_ID)
 
     async def get_punishment_image(self, member: discord.Member):
         query = 'SELECT url FROM punishment_images WHERE user_id = $1'
@@ -54,6 +60,43 @@ class Mod(commands.Cog):
         else:
             await ctx.reply(title=f'{member} foi expulso com sucesso', image=image)
             self.bot.dispatch('moderation_command', 'kick', ctx, member)
+
+    @commands.command(aliases=['m'])
+    @commands.has_guild_permissions(mute_members=True)
+    async def mute(self, ctx: commands.Context, member: discord.Member, *, when: FutureTime):
+        reminder = ctx.bot.get_cog('Reminder')
+        if not reminder:
+            return await ctx.reply('Serviço indisponível, tente novamente mais tarde.')
+
+        await member.add_roles(self.mute_role, reason=f'Ação realizada por {ctx.author} (ID: {ctx.author.id}')
+        timer = await reminder.create_timer(
+            when.datetime,
+            'mute',
+            ctx.author.id,
+            member.id,
+            created=ctx.message.created_at
+        )
+
+        image = await self.get_punishment_image(ctx.author)
+        delta = humanize.precisedelta(when.datetime - ctx.message.created_at, format='%0.0f')
+
+        await ctx.reply(title=f'{member} foi silenciado por {delta}', image=image)
+        self.bot.dispatch('moderation_command', 'mute', ctx, member, duration=delta)
+
+    @commands.Cog.listener()
+    async def on_mute_complete(self, timer: Timer):
+        moderator_id, member_id = timer.args
+        await self.bot.wait_until_ready()
+
+        member = self.cosmic.get_member(member_id)
+        moderator = self.cosmic.get_member(moderator_id)
+
+        reason = f'Removendo silenciamento realizado por {moderator}'
+
+        try:
+            await member.remove_roles(self.mute_role, reason=reason)
+        except discord.HTTPException:
+            pass
 
     @commands.Cog.listener()
     async def on_moderation_command(self, name: str, ctx: commands.Context, member: discord.Member, **kwargs):
